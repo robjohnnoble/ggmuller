70d886440246a98a62e3a6b24bc4e286a4de79e5
Rob Noble <robjohnnoble@gmail.com>
2016-06-21 10:47:42 +0200

    Better error check in get_path()
    Previous error check precluded data sets with more than 1E6 genotypes.

--- R/MullerPlot.R
+++ R/MullerPlot.R
@@ -138,7 +138,7 @@ get_path <- function(edges) {
       upped <- TRUE
     }
     if(path[n] == path[1]) break
-    if(n > 1E6) stop("Error: stuck in a loop")
+    if(n > 2 * dim(edges)[1] + 2) stop("Error: stuck in a loop")
     if(max(table(path) > 2)) stop("Error: adjacency matrix seems to include loops.")
   }
   if(length(path) != 2 * dim(edges)[1] + 2) stop("Error: adjacency matrix seems to be bipartite.")
